7f94416041a896636759c72521ca992518e813a4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-12-05 14:12:34 +0200

    Removed the part as it is no longer needed

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-pub fn read_input(day: u8, part: u8) -> String {
-    fs::read_to_string(format!("./src/day{}/part{}/input.txt", day, part))
+pub fn read_input(day: u8) -> String {
+    fs::read_to_string(format!("./src/day{}/input.txt", day))
         .expect("Should have been able to read the file")
 }
